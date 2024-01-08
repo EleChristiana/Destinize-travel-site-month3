545e9bbe3e484399940af7f41c48272a810b0450
EleChristiana <120030329+EleChristiana@users.noreply.github.com>
2024-01-07 22:04:48 -0800

    Update functionloop.js

--- functionloop.js
+++ functionloop.js
@@ -1,10 +1,1 @@
-function input(){
-    let arr = ["orange", "apple", "banana" ,"pineaple", "spinach"]
-    for(let i = 0; i < arr.length; i++){
-        console.log(arr[i]);
-    }
-    
-}
-
-input();
 
